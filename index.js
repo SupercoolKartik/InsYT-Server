4f55c556f2ff2b24fcb8f9c8ef8cff104a2bae85
Suraj <surajkartik71@gmail.com>
2024-05-05 16:08:21 +0530

    Added a function to GET THE PLAYLIST TITLE & removed the time duration format function

--- index.js
+++ index.js
@@ -1,47 +1,43 @@
 import http from "http";
 import express from "express";
-
+import { google } from "googleapis";
 import dotenv from "dotenv";
+
 const app = express();
 const server = http.createServer(app);
 const PORT = 5003;
-import { google } from "googleapis";
 
 // Calling dotenv.config()
 dotenv.config();
 
 const apikey = process.env.API_KEY;
 
-//FUNCTION TO FORMAT THE DURATION
-function formatTime(seconds) {
-  // Calculate hours, minutes, and seconds
-  let hours = Math.floor(seconds / 3600);
-  let minutes = Math.floor((seconds % 3600) / 60);
-  let remainingSeconds = seconds % 60;
-
-  // Initialize an empty array to store the time components
-  let timeComponents = [];
-
-  // Add hours, minutes, and seconds to the array if they are not zero
-  if (hours > 0) {
-    timeComponents.push(hours < 10 ? `0${hours}` : hours);
-  }
-  if (minutes > 0 || hours > 0) {
-    timeComponents.push(minutes < 10 ? `0${minutes}` : minutes);
-  }
-  timeComponents.push(
-    remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds
-  );
-
-  // Join the time components with colons
-  return timeComponents.join(":");
-}
-
-//FUNCTION TO GET VIDEO DETAILS
-const getVideoLength = async (apiKey, videoId) => {
+//FUNCTION TO GET THE PLAYLIST NAME
+const getPlaylistTitle = async (auth, playlistId) => {
   const youtube = google.youtube({
     version: "v3",
-    auth: apiKey,
+    auth,
+  });
+
+  // Define the request parameters
+  const request_parameters = {
+    part: "snippet",
+    id: playlistId,
+  };
+  try {
+    const response = await youtube.playlists.list(request_parameters);
+    //const response = await youtube.playlists().list(playlistId).execute();
+    return await response.data.items[0].snippet.title;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//FUNCTION TO GET VIDEO DETAILS
+const getVideoLength = async (auth, videoId) => {
+  const youtube = google.youtube({
+    version: "v3",
+    auth,
   });
 
   try {
@@ -49,14 +45,14 @@ const getVideoLength = async (apiKey, videoId) => {
       part: "contentDetails",
       id: videoId,
     });
-    const duration = response.data.items[0].contentDetails.duration;
+    const duration = response.data.items[0].contentDetails.duration; //Duration of Video(of a particular video)
 
     //Converting the duration
     const match = duration.match(/PT(?:(\d+)H)?(?:(\d+)M)?(\d+)S/);
     const hours = parseInt(match[1] || 0, 10);
     const minutes = parseInt(match[2] || 0, 10);
     const seconds = parseInt(match[3], 10);
-    const parsedMinutes = hours * 60 + minutes;
+
     const parsedSec = hours * 60 * 60 + minutes * 60 + seconds;
     return parsedSec;
   } catch (error) {
@@ -86,35 +82,28 @@ const listPlaylistItems = async (auth, playlistId) => {
 };
 
 app.get("/", async (req, res) => {
-  const pId = req.query.pId;
+  const pId = req.query.pId; //Playlist Id
 
-  const items = await listPlaylistItems(apikey, pId);
-  console.log("ITEMS LIST STARTS HERE:-");
+  const items = await listPlaylistItems(apikey, pId); //List of Playlist items
 
   // Create an array of promises for video length retrievals
   const videoLengthPromises = items.map(async (item) => {
-    const vId = item.snippet.resourceId.videoId;
+    const vId = item.snippet.resourceId.videoId; //Video Id(of a particular video in the playlist)
     return await getVideoLength(apikey, vId);
   });
-  //console.log("total", totalLen);
-
   // Wait for all video length promises to resolve
   const videoLengths = await Promise.all(videoLengthPromises);
-
   //Calculate the total duration
-  let totalSec = 0;
+  let playlist_length = 0; //(in seconds)
   for (let i = 0; i < videoLengths.length; i++) {
-    console.log("Length of video " + i + " " + videoLengths[i]);
-    totalSec += videoLengths[i];
+    playlist_length += videoLengths[i];
   }
-  const formatedLength = formatTime(totalSec);
 
-  console.log("length:-", items.length);
-
-  console.log("Duration", formatedLength);
+  const playlist_size = items.length;
+  const playlist_name = await getPlaylistTitle(apikey, pId);
 
   // Send the response with the items and total duration
-  res.send({ items, totalSec });
+  res.json({ playlist_name, playlist_size, playlist_length });
 });
 
 server.listen(PORT, () => {
